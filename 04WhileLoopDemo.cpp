c049c9aea5417c0246ca5309e81889d6e4f10680
Paul Raupach <paul@raupach.com>
2022-01-31 11:41:29 -0800

    First commit

--- 04WhileLoopDemo.cpp
+++ 04WhileLoopDemo.cpp
@@ -10,33 +10,13 @@ using namespace std;
 int main()
 {
 //declare variables for starting number and the ending number
-    int startingNumber;
-    int endingNumber;
+
 //prompt user for variables
-
-    cout << "Enter the starting number: ";
-    cin>> startingNumber;
-    cout << endl;  //newline
-
-    cout << "Enter the ending number: ";
-    cin >> endingNumber;
 
 // define "current number" variable and set current number equal
 //to the starting number
 
-    int currentNumber = startingNumber;
-
 //use a loop to output the values
-
-    cout << "The numbers between "  << startingNumber << " and "
-         << endingNumber << " are: \n";
-
-
-    while (currentNumber <= endingNumber)
-    {
-        cout << currentNumber << endl;  //output current number on  new line
-        currentNumber+=2;  //increment current number
-    }
 
 //end the program
 
